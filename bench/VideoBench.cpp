f190523dd6ab4688e9de633be3db5a6405aa8d90
StephanXu <xuzihanapple@live.com>
2020-02-22 13:06:03 +0800

    添加benchmark
    - 添加模板类实现与虚函数实现测试

--- bench/VideoBench.cpp
+++ bench/VideoBench.cpp
@@ -4,91 +4,178 @@
 #include <cmath>
 #include <unordered_map>
 
-static void BMPow(benchmark::State &state)
+static void BMPow(benchmark::State& state)
 {
-    for (auto _ : state)
-    {
-        //std::vector<int> v(state.range(0), state.range(0));
-        int result = std::pow(state.range(0), 2);
-        //benchmark::DoNotOptimize(v.data());
-        //benchmark::ClobberMemory();
-    }
+	for (auto _ : state)
+	{
+		//std::vector<int> v(state.range(0), state.range(0));
+		int result = std::pow(state.range(0), 2);
+		//benchmark::DoNotOptimize(v.data());
+		//benchmark::ClobberMemory();
+	}
 }
 
-static void BMNativeTime(benchmark::State &state)
+static void BMNativeTime(benchmark::State& state)
 {
-    for (auto _ : state)
-    {
-        //std::vector<int> v(state.range(0), state.range(0));
-        int result = state.range(0) * state.range(0);
-        //benchmark::DoNotOptimize(v.data());
-        //benchmark::ClobberMemory();
-    }
+	for (auto _ : state)
+	{
+		//std::vector<int> v(state.range(0), state.range(0));
+		int result = state.range(0) * state.range(0);
+		//benchmark::DoNotOptimize(v.data());
+		//benchmark::ClobberMemory();
+	}
 }
 
-static void BMProtobufParam(benchmark::State &state)
+static void BMProtobufParam(benchmark::State& state)
 {
-    OssianConfig::Configuration config;
-    auto aimbot = config.mutable_aimbot();
-    aimbot->set_areanormalizedbase(1.25);
-    for (auto _ : state)
-    {
-        float result = aimbot->areanormalizedbase() + 202;
-    }
+	OssianConfig::Configuration config;
+	auto aimbot = config.mutable_aimbot();
+	aimbot->set_areanormalizedbase(1.25);
+	for (auto _ : state)
+	{
+		float result = aimbot->areanormalizedbase() + 202;
+	}
 }
 
 struct config
 {
-    float aimbot;
+	float aimbot;
 };
 
-static void BMNoProtobufParam(benchmark::State &state)
+static void BMNoProtobufParam(benchmark::State& state)
 {
-    config c;
-    c.aimbot = 100;
-    for (auto _ : state)
-    {
-        float result = c.aimbot + 202;
-    }
+	config c;
+	c.aimbot = 100;
+	for (auto _ : state)
+	{
+		float result = c.aimbot + 202;
+	}
 }
 
-static void BMIntHashMap(benchmark::State &state)
+static void BMIntHashMap(benchmark::State& state)
 {
-    std::unordered_map<int, int> map;
-    map.insert(std::make_pair(1, 2));
-    map.insert(std::make_pair(2, 3));
-    map.insert(std::make_pair(3, 4));
-    for (auto _ : state)
-    {
-        float res = map.find(2)->second + 10;
-    }
+	std::unordered_map<int, int> map;
+	map.insert(std::make_pair(1, 2));
+	map.insert(std::make_pair(2, 3));
+	map.insert(std::make_pair(3, 4));
+	for (auto _ : state)
+	{
+		float res = map.find(2)->second + 10;
+	}
 }
 
-static void BMStringHashMap(benchmark::State &state)
+static void BMStringHashMap(benchmark::State& state)
 {
-    std::unordered_map<std::string, int> map;
-    map.insert(std::make_pair("1", 2));
-    map.insert(std::make_pair("2", 3));
-    map.insert(std::make_pair("3", 4));
-    for (auto _ : state)
-    {
-        float res = map.find("2")->second + 10;
-    }
+	std::unordered_map<std::string, int> map;
+	map.insert(std::make_pair("1", 2));
+	map.insert(std::make_pair("2", 3));
+	map.insert(std::make_pair("3", 4));
+	for (auto _ : state)
+	{
+		float res = map.find("2")->second + 10;
+	}
 }
 
-static void BMSharedPtrHashMap(benchmark::State &state)
+static void BMSharedPtrHashMap(benchmark::State& state)
 {
-    std::unordered_map<std::shared_ptr<int>, int> map;
-    auto ptr1 = std::make_shared<int>(1);
-    auto ptr2 = std::make_shared<int>(2);
-    auto ptr3 = std::make_shared<int>(3);
-    map.insert(std::make_pair(ptr1, 2));
-    map.insert(std::make_pair(ptr2, 3));
-    map.insert(std::make_pair(ptr3, 4));
-    for (auto _ : state)
-    {
-        float res = map.find(ptr3)->second + 10;
-    }
+	std::unordered_map<std::shared_ptr<int>, int> map;
+	auto ptr1 = std::make_shared<int>(1);
+	auto ptr2 = std::make_shared<int>(2);
+	auto ptr3 = std::make_shared<int>(3);
+	map.insert(std::make_pair(ptr1, 2));
+	map.insert(std::make_pair(ptr2, 3));
+	map.insert(std::make_pair(ptr3, 4));
+	for (auto _ : state)
+	{
+		float res = map.find(ptr3)->second + 10;
+	}
+}
+
+class IInterface
+{
+public:
+	virtual auto GetStatus(int foo)->int = 0;
+};
+
+class AddImpl final :public IInterface
+{
+public:
+	auto GetStatus(int foo)->int override
+	{
+		return foo + 100;
+	}
+};
+
+class MinusImpl final :public IInterface
+{
+public:
+	auto GetStatus(int foo)->int override
+	{
+		return foo - 100;
+	}
+};
+
+std::unique_ptr<IInterface> CreateImpl(std::string bar)
+{
+	if (bar.compare("add") == 0)
+	{
+		return std::make_unique<AddImpl>();
+	}
+	else
+	{
+		return std::make_unique<MinusImpl>();
+	}
+}
+
+struct AddTemplateImpl
+{
+	auto GetStatus(int foo) { return foo + 100; }
+};
+
+struct MinusTemplateImpl
+{
+	auto GetStatus(int foo) { return foo - 100; }
+};
+
+template<class Operation>
+class TemplateInterface : public IInterface
+{
+	Operation m_Operation;
+public:
+	auto GetStatus(int bar)->int final
+	{
+		return m_Operation.GetStatus(bar);
+	}
+};
+
+std::unique_ptr<IInterface> CreateTemplateImpl(std::string bar)
+{
+	if (bar.compare("add") == 0)
+	{
+		return std::make_unique<TemplateInterface<AddTemplateImpl>>();
+	}
+	else
+	{
+		return std::make_unique<TemplateInterface<MinusTemplateImpl>>();
+	}
+}
+
+static void BMVirtualFunctionCall(benchmark::State& state)
+{
+	std::unique_ptr<IInterface> obj = CreateImpl("add");
+	for (auto _ : state)
+	{
+		auto result = obj->GetStatus(1000);
+	}
+}
+
+static void BMTemplateCall(benchmark::State& state)
+{
+	std::unique_ptr<IInterface> obj = CreateTemplateImpl("add");
+	for (auto _ : state)
+	{
+		auto result = obj->GetStatus(1000);
+	}
 }
 
 BENCHMARK(BMProtobufParam);
@@ -100,3 +187,6 @@ BENCHMARK(BMNativeTime)->Arg(8);
 BENCHMARK(BMIntHashMap);
 BENCHMARK(BMStringHashMap);
 BENCHMARK(BMSharedPtrHashMap);
+
+BENCHMARK(BMVirtualFunctionCall);
+BENCHMARK(BMTemplateCall);
